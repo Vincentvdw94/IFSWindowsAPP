f3db028ba2762cc3c88d6a7453f942241b75d22e
Vincent Vandewyer <vincent.vandewyer@student.hogent.be>
2020-07-22 10:22:15 +0200

    add repos to scope.

--- backend/ifes/ifes/Extensions/StartupExtensions.cs
+++ backend/ifes/ifes/Extensions/StartupExtensions.cs
@@ -12,6 +12,11 @@ namespace ifes.Extensions {
         public static void RegisterRepositories(this IServiceCollection services) {
 
             services.AddScoped(typeof(IRepository<Food>), typeof(Repository<Food>));
+            services.AddScoped(typeof(IRepository<Beverage>), typeof(Repository<Beverage>));
+            services.AddScoped(typeof(IRepository<Seat>), typeof(Repository<Seat>));
+            services.AddScoped(typeof(IRepository<Passenger>), typeof(Repository<Passenger>));
+            services.AddScoped(typeof(IRepository<Audio>), typeof(Repository<Audio>));
+            services.AddScoped(typeof(IRepository<Video>), typeof(Repository<Video>));
 
         }
 
